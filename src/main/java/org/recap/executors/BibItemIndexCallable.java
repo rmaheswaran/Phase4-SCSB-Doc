c5fd57a24344893487b40bc5e3c32259a7e55d09
Hari-Krishnan19 <krish19nov@gmail.com>
2017-07-31 19:13:30 +0530

    Fixed bug to display logs

--- src/main/java/org/recap/executors/BibItemIndexCallable.java
+++ src/main/java/org/recap/executors/BibItemIndexCallable.java
@@ -100,7 +100,7 @@ public class BibItemIndexCallable implements Callable {
             futures.add(submit);
         }
 
-        logger.info("Num futures to prepare Bib and Associated data : ",futures.size());
+        logger.info("Num futures to prepare Bib and Associated data : {} ",futures.size());
 
         List<SolrInputDocument> solrInputDocumentsToIndex = new ArrayList<>();
         for (Iterator<Future> futureIterator = futures.iterator(); futureIterator.hasNext(); ) {
