f5452b06f62fda12e48f11c09c4cc071250e1bad
rajeshbabuk <rajeshbabu.k@htcindia.com>
2021-05-25 10:04:15 +0530

    SCSB-592: Added api's for batch scheduler to get job details.

--- src/test/java/org/recap/controller/UpdateJobControllerUT.java
+++ src/test/java/org/recap/controller/UpdateJobControllerUT.java
@@ -6,10 +6,15 @@ import org.mockito.Mock;
 import org.recap.BaseTestCaseUT;
 import org.recap.ScsbCommonConstants;
 import org.recap.ScsbConstants;
+import org.recap.model.job.JobDto;
+import org.recap.model.job.JobParamDto;
 import org.recap.model.jpa.JobEntity;
 import org.recap.repository.jpa.JobDetailsRepository;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by rajeshbabuk on 20/4/17.
@@ -28,5 +33,23 @@ public class UpdateJobControllerUT extends BaseTestCaseUT {
         jobEntity.setJobName(ScsbConstants.PURGE_EXCEPTION_REQUESTS);
         String status=updateJobController.updateJob(jobEntity);
         assertEquals(ScsbCommonConstants.SUCCESS,status);
+    }
+
+    @Test
+    public void testGetAllJobs() throws Exception {
+        List<JobDto> jobDtoList = updateJobController.getAllJobs();
+        assertNotNull(jobDtoList);
+    }
+
+    @Test
+    public void testGetJobByName() throws Exception {
+        JobDto job = updateJobController.getJobByName(ScsbConstants.PURGE_EXCEPTION_REQUESTS);
+        assertNotNull(job);
+    }
+
+    @Test
+    public void testGetJobParamsByJobName() throws Exception {
+        JobParamDto jobParamDto = updateJobController.getJobParamsByJobName(ScsbConstants.GENERATE_ACCESSION_REPORT_JOB);
+        assertNotNull(jobParamDto);
     }
 }
